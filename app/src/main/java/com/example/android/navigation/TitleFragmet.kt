5d967e291d62f068322363be529b3a323290d2b1
TARUMT <tkairen7@gmail.com>
2023-04-14 14:52:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/android/navigation/TitleFragmet.kt
@@ -0,0 +1,40 @@
+package com.example.android.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [TitleFragmet.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class TitleFragmet : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
+        R.layout.fragment_title,container,false)
+
+        binding.playButton.setOnClickListener{view:View ->
+            view.findNavContoller().navigate(R.id.action_titleFragment_to_gameFragment.title)
+        }
+        return binding.root
+    }
+
+}
+
